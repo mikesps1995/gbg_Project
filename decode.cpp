892e61c16f19a5ec87fca576e88f724a4adc9a59
Egan-m <mike-sps1995@gmail.com>
2016-12-08 16:40:06 -0500

    added some notes and named the button flag set

--- decode.cpp
+++ decode.cpp
@@ -9,15 +9,26 @@
 #include "gbg.h"
 boolean relayStatus = false;
 
+/**************************************************************************
+ * The code, also transferred from phone as buttonStatus is a 1 bit
+ * shifted left by the button number.  This way two or more buttons
+ * can be simultaneously transferred.  That works from the phone to the
+ * Arduino BT.
+ * Since there are 8 buttons we'll define them.  Defs can change anytime.
+**************************************************************************/
 
 int decode(int code) 
 {
     int rotator = 0;
     int flags = 0;
 /**************************************************************************
- * decide what to shut off, if anything on a zero code.
+ * tbd decide what to shut off, if anything on a zero code.
  * tbd this needs to handle combinations of the switches if two or more
  * are simultaneously invoked.
+ *
+ * The rotator is a 1 shifted left by a counter.  At each count, if the
+ * result of 'and' with the code the switch is run.  Switch decodes the
+ * particular bit.
 **************************************************************************/
     Serial.println(code, HEX);
     
@@ -28,7 +39,8 @@ int decode(int code)
             digitalWrite(RUN_RELAY_DRV, 0);
         }
     }
-        
+
+    
     for(rotator = 0; rotator < 8; rotator++) {
         flags = 0xffff & (code & (1 << rotator));
         if(flags != 0) {
@@ -36,23 +48,22 @@ int decode(int code)
             Serial.println(flags, HEX);
             switch(flags) {
                 case 0x02:
-                    Serial.println("action = 1");
-                    
+                    Serial.println(" ACT_FWD ");
                     break;
                 case 0x04:
-                    Serial.println("action = 2");
+                    Serial.println(" ACT_SLOWER ");
                     break;
                 case 0x08:
-                    Serial.println("action = 3");
+                    Serial.println(" ACT_STOP ");
                     break;
                 case 0x10:
-                    Serial.println("action = 4");
+                    Serial.println(" ACT_START ");
                     break;
                 case 0x20:
-                    Serial.println("action = 5");
+                    Serial.println(" ACT_FASTER ");
                     break;
                 case 0x40:
-                    Serial.println("action = 6");
+                    Serial.println(" ACT_REV ");
                     if(relayStatus == false) {
                         relayStatus = true;
                         digitalWrite(REV_RELAY_DRV, 1);
@@ -60,11 +71,11 @@ int decode(int code)
                     }
                     break;
                 case 0x80:
-                    Serial.println("action = 7");
+                    Serial.println(" ACT_LIGHT ");
                     break;
                     
                 case 0x100:
-                    Serial.println("action = 8");
+                    Serial.println(" ACT_HORN ");
                     break;
                 default:
                     break;
